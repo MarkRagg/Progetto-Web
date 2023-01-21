c01e139a943de82ee6eb99c5645dd1f9ebaf97d4
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2023-01-21 17:04:52 +0100

    add first example of Succesfully Sign-in

--- source/php/Sign-in.php
+++ source/php/Sign-in.php
@@ -3,3 +3,27 @@ require_once("db_config.php");
 $query_result = $dbh->AddUser($_POST["nickname"], $_POST["email"], $_POST["password"], $_POST["name"], $_POST["surname"], $_POST["date"], $_POST["residence"]);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="eng">
+  <head>
+
+  </head>
+  <body>
+    <header>
+    </header>
+    <main>
+      <section>
+        <?php if ($query_result == true) {
+          echo "<h1>Registrazione completata!</h1>";
+        }
+        else {
+          echo "<h1>Registrazione fallita!</h1>";
+        } ?>  
+      </section>
+    </main>
+    <footer>
+      <!--TODO--> 
+    </footer>
+  </body>
+</html>

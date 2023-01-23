9b54d3f6d707540ade972c51d5877013f863753b
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2023-01-23 19:57:11 +0100

    add javascript for sign-in

--- source/js/sign-in.js
+++ source/js/sign-in.js
@@ -1,3 +1,29 @@
+function generateForm(loginerror = null) {
+  let form = `
+  <section>
+    <button type="button" onclick="location.href='../php/Login.php';">Log in</button><button type="button" disabled>Sign in</button>
+    <form action="../php/Sign-in.php" method="POST">
+    <ul>
+      <li><label for="nickname">Nickname</label><input type="text" id="nickname" name="nickname"/></li>
+      <li><label for="email">Email</label><input type="email" id ="email" name="email"/></li>
+      <li><label for="password">Password</label><input type="password" id="password" name="password"/></li>
+      <li><label for="name">Name</label><input type="text" id="name" name="name"/></li>
+      <li><label for="surname">Surname</label><input type="text" id="surname" name="surname"/></li>
+      <li><label for="date">Date</label><input type="date" id="date" name="date"/></li>
+      <li><label for="residence">Residence</label>
+        <select id="residence" name="residence">
+          <option value="Example">Example</option>
+          </select>
+        </li>
+    </ul>
+    <button type="submit">Register</button>
+    <p class="text-danger"></p>
+    </form>
+  </section>
+  `;
+  return form;
+}
+
 function generateSuccesfullSignin(loginerror = null) {
   let form = `
   <section>
@@ -6,33 +32,57 @@ function generateSuccesfullSignin(loginerror = null) {
   return form;
 }
 
-function generateError(loginerror = null) {
-  let form = `
-  <label class="text-danger">Nickname already existed! Retry.</label>`;
-  return form;
-}
-
 function visualizeSuccess() {
   section = generateSuccesfullSignin();
   main.innerHTML = section;
 }
 
-function visualizeError() {
-  error = generateError();
-  main.innerHTML += error;
-}
-
 const main = document.querySelector("main");
-console.log("ciao");
 axios.get('Sign-in.php').then(response => {
-    console.log(response);
-    console.log(response.data["sign-in-result"]);
     if (response.data["sign-in-result"]) {
         // User Sign-in succesfully
-        console.log("ciao");
         visualizeSuccess();
     } else {
         // Utente NON loggato
-        visualizeError();
+        VisualizeSigninForm();  
     }
 });
+
+
+function VisualizeSigninForm() {
+  // Utente NON loggato
+  let form = generateForm();
+  main.innerHTML = form;
+  // Gestisco tentativo di login
+  document.querySelector("main form").addEventListener("submit", function (event) {
+      event.preventDefault();
+      const nickname = document.querySelector("#nickname").value;
+      const email = document.querySelector("#email").value;
+      const name = document.querySelector("#name").value;
+      const surname = document.querySelector("#surname").value;
+      const password = document.querySelector("#password").value;
+      const date = document.querySelector("#date").value;
+      const residence = document.querySelector("#residence").value;
+      signin(nickname, email, name, surname, password, date, residence);
+  });
+}
+
+function signin(nickname, email, name, surname, password, date, residence) {
+  const formData = new FormData();
+
+  formData.append('nickname', nickname);
+  formData.append('email', email);
+  formData.append('name', name);
+  formData.append('surname', surname);
+  formData.append('date', date);
+  formData.append('password', password);
+  formData.append('residence', residence);
+
+  axios.post('Sign-in.php', formData).then(response => {
+      if (response.data["sign-in-result"]) {
+        visualizeSuccess();
+      } else {
+        document.querySelector("form > p").innerText = response.data["text-error"];
+      }
+  });
+}
--- source/php/Sign-in.php
+++ source/php/Sign-in.php
@@ -2,12 +2,14 @@
 require_once("db_config.php");
 
 $result["sign-in-result"] = false;
+$result["text-error"] = "";
 
 if(isset($_POST["nickname"]) && isset($_POST["email"]) && isset($_POST["password"])  && isset($_POST["name"]) && isset($_POST["surname"]) && isset($_POST["date"]) && isset($_POST["residence"])) {
   if($dbh->checkValueInDb("user", "user_id", $_POST["nickname"])) {
     $result["sign-in-result"] = $dbh->addUser($_POST["nickname"], $_POST["email"], $_POST["password"], $_POST["name"], $_POST["surname"], $_POST["date"], $_POST["residence"]);
   } else {
     $result["sign-in-result"] = false;
+    $result["text-error"] = "Nickname already exists! Retry.";
   }
 }
 

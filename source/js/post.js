5f53c2b3665aae0ccbcf0dcac12e0fb3ecf49698
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-01-24 17:32:17 +0100

    better view

--- source/js/post.js
+++ source/js/post.js
@@ -2,35 +2,34 @@ function generatePost(post_data){
     let section = 
     `
     <section>
-       
-    </section>
-`
-for (let i = 0; i < post_data.length; i++){
-    section += `
+    `
+    for (let i = 0; i < post_data.length; i++){
+        section += `
 
-        <div class="container mt-4 mb-5">
-            <div class="d-flex justify-content-center row">
-                <div class="col-md-8">
-                    <div class="feed p-2">
-                        <div class="bg-white border mt-2">
-                            <div>
-                                <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
-                                    <div class="d-flex flex-row align-items-center feed-text px-2"><img src="${post_data[i]["user_image"]}" alt=""/>
-                                        <div class="d-flex flex-column flex-wrap ml-2"><a class="nav-link" href="profile.php"> ${post_data[i]["author"]} </a></div>
+            <div class="container mt-4 mb-5">
+                <div class="d-flex justify-content-center row">
+                    <div class="col-md-8">
+                        <div class="feed p-2">
+                            <div class="bg-white border mt-2">
+                                <div>
+                                    <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
+                                        <div class="d-flex flex-row align-items-center feed-text px-2"><img src="${post_data[i]["user_image"]}" alt=""/>
+                                            <div class="d-flex flex-column flex-wrap ml-2"><a class="nav-link" href="profile.php"> ${post_data[i]["author"]} </a></div>
+                                        </div>
+                                        <div class="feed-icon px-2"><em class="fa fa-ellipsis-v text-black-50"></em></div>
                                     </div>
-                                    <div class="feed-icon px-2"><em class="fa fa-ellipsis-v text-black-50"></em></div>
                                 </div>
+                                <div class="p-2 px-3"><span>${post_data[i]["string"]}</span></div>
+                                <div class="d-flex justify-content-end socials p-2 py-3"><em class="fa fa-thumbs-up">LIKE</em><em class="fa fa-comments-o">COMMENTO</em><em class="fa fa-share">REACTION</em></div>
                             </div>
-                            <div class="p-2 px-3"><span>${post_data[i]["string"]}</span></div>
-                            <div class="d-flex justify-content-end socials p-2 py-3"><em class="fa fa-thumbs-up">LIKE</em><em class="fa fa-comments-o">COMMENTO</em><em class="fa fa-share">REACTION</em></div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-
-    `
-}
+        
+        `
+    }
+    section += `</section>`;
 return section;
 }
 

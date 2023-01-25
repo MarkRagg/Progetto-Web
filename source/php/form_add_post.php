93f11753c27982f2cb9b4b50994c5f99675e50b7
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-01-25 22:35:15 +0100

    better view

--- source/php/form_add_post.php
+++ source/php/form_add_post.php
@@ -1,11 +1,19 @@
 <section>
+    <div class="d-flex justify-content-center ">
+    <div class="flex-column border">
+        <div class="p-5">
+        <p class="text-danger"> <?php echo $error; ?> </p>
         <form action="insertpost.php" method="POST" enctype="multipart/form-data">
-            <h2>Insert post</h2>
-            <label for="post">Post</label>
-            <input type="text" name="post" id="post">
-            <input type="submit" name="submit" value="Posta">
-            <a href="../php/showhomepage.php">Back</a>
+        <label for="post">Il Tuo Nuovo Post</label>
+            <hr />
+            <textarea class="form-control" id="post" name="post" rows="4"></textarea>
+            <small id="passwordHelpBlock" class="form-text text-muted">
+                Il post puo' essere lungo massimo 200 caratteri.</small>
+            <hr />
+            <input class="align-self-end" type="submit" name="submit" value="Posta">
         </form>
-        <p> <?php echo $error; ?> </p>
+        </div>
+    </div>
+    </div>
 </section>
 

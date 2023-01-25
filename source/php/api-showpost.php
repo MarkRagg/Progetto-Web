d84ed2cb49ee5636a1d4a7c86e922290a4c403d9
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-01-25 12:15:13 +0100

    remove usless code

--- source/php/api-showpost.php
+++ source/php/api-showpost.php
@@ -2,9 +2,6 @@
 require_once("db_config.php");
 
 $post = $dbh->getPosts(3);
-/*$result["username"] = $post["author"];
-$result["user_image"] = $image["user_image"];
-$result["contenuto"]=$post["string"];*/
 
 header("Content-Type: application/json");
 echo json_encode($post);

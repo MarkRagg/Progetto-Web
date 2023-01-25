429304d54c56ac035b3d7d2d94fd87c675afccb2
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-01-25 17:41:31 +0100

    better implementation

--- source/php/insertpost.php
+++ source/php/insertpost.php
@@ -6,12 +6,15 @@ $templateParams["name"] = "form_add_post.php";
 
 $testo = $_POST["post"];
 
-if ($testo != "" && isset($_SESSION["user_id"])) {
-    $dbh->addPost($testo, $_SESSION["user_id"]);
-    header("Location: showhomepage.php");
-} else {
-    var_dump("Errore");
+if(isset($_POST["submit"])){
+    if ($testo != "" && isset($_SESSION["user_id"])) {
+        $dbh->addPost($testo, $_SESSION["user_id"]);
+        header("Location: showhomepage.php");
+    } else {
+        var_dump("Errore");
+    }
 }
+
 
 require '../template/base.php';
 ?>

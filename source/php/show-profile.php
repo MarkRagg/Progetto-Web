16f5530e0dcca7ef1f9921cd7c0753145a85be0d
Simone Zama <simone.zama@studio.unibo.it>
2023-01-25 14:38:40 +0100

    Fix, show username correctly

--- source/php/show-profile.php
+++ source/php/show-profile.php
@@ -1,8 +1,9 @@
 <?php
 if ($templateParams["user_exists"]) {
-    $username = $templateParams["u_name"];
-    $name = $templateParams["user_image"];
+    $name = $templateParams["u_name"];
+    $username = $templateParams["username"];
     $surname = $templateParams["surname"];
+    $user_image = $templateParams["user_image"];
     $date_of_birth = $templateParams["date_of_birth"];
     $residence = $templateParams["residence"];
     $corso = $templateParams["corso_id"];
@@ -17,7 +18,7 @@ if ($templateParams["user_exists"]) {
         </div>
         <div class="container text-center">
             <div class="row">
-                <h1 class="col">$username $surname</h1>
+                <h1 class="col">$name $surname</h1>
             </div>
             <div class="row">
                 <h2 class="col text-muted">$username</h2>
@@ -32,12 +33,14 @@ if ($templateParams["user_exists"]) {
             </div>
             <div class="row">
                 <ul class="list-group list-group-horizontal">
-                    <li class="col-4 list-group-item align-items-center h4"><a href="#">Post</a> <span class="badge bg-primary rounded-pill">$post_count</span></li>
-                    <li class="col-4 list-group-item align-items-center h4"><a href="#">Seguaci</a> <span class="badge bg-primary rounded-pill">$follower_count</span></li>
-                    <li class="col-4 list-group-item align-items-center h4"><a href="#">Seguiti</a> <span class="badge bg-primary rounded-pill">$followed_count</span></li>
+                    <li class="col-4 list-group-item align-items-center h4"><a id="posts" href="#">Post</a> <span class="badge bg-primary rounded-pill">$post_count</span></li>
+                    <li class="col-4 list-group-item align-items-center h4"><a id="followers" href="#">Seguaci</a> <span class="badge bg-primary rounded-pill">$follower_count</span></li>
+                    <li class="col-4 list-group-item align-items-center h4"><a id="following" href="#">Seguiti</a> <span class="badge bg-primary rounded-pill">$followed_count</span></li>
                 </ul>
             </div>
         </div>
+        <script src="../js/user-list.js"></script>
+        <script src="https://unpkg.com/axios/dist/axios.min.js">
     </section>
     EOD;
 } else {

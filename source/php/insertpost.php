da115c04e57970ff6bdadc794697cf065cf06048
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-01-25 12:37:05 +0100

    Update insertpost.php

--- source/php/insertpost.php
+++ source/php/insertpost.php
@@ -1,8 +1,8 @@
 <?php
 
-$user = $_SESSION["user_id"];
+
 $templateParams["title"] = "Create a new post";
 $templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","../js/postform.js");
 
-require '../template/Login-base.php';
+require '../template/base.php';
 ?>

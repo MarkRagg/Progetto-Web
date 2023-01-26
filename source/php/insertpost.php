d65943fce5ec48ba433411ba8db77afc179a0f6a
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2023-01-26 16:19:34 +0100

    fix bug

--- source/php/insertpost.php
+++ source/php/insertpost.php
@@ -5,9 +5,10 @@ $templateParams["title"] = "Insert Post";
 $templateParams["name"] = "../template/form_add_post.php";
 $templateParams["paginaprofilouser"]= $_SESSION["user_id"];
 
-$testo = $_POST["post"];
+$error = "";
 
-if(isset($_POST["submit"])){
+if(isset($_POST["submit"]) && isset($_POST["post"])){
+    $testo = $_POST["post"];
     if ($testo != "" && isset($_SESSION["user_id"])) {
         $dbh->addPost($testo, $_SESSION["user_id"]);
         header("Location: showhomepage.php");

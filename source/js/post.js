d75b2e20e8fb49d7937137de459faa33c2921683
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-01-31 20:33:30 +0100

    added quickpost

--- source/js/post.js
+++ source/js/post.js
@@ -1,7 +1,7 @@
 //${post_data[i]["user_image"]}
 
-function generatePost(post_data){
-    let section = `<div class="container mt-2 mb-5">
+function generatePost(post_data) {
+  let section = `<div class="container mt-2 mb-5">
     <div class="row">
       <div class="col-12 col-lg-3">
         <div class="left-column">
@@ -32,13 +32,16 @@ function generatePost(post_data){
       <div class="col-12 col-lg-6">
         <div class="middle-column">
           <div class="card border-primary">
-            <div class="card-header bg-primary">
-              <h3>Homepage</h3>
-              <button class="btn btn-secondary float-right" onclick="location.href='../php/insertpost.php';">Aggiungi
-                post</button>
+            <div class="card-header">
+                <div class="input-group">
+                  <input type="text" class="quickpost form-control mx-3" placeholder="Post veloce">
+                  <div class="input-group-append">
+                    <button type="submit" class="bttnpost btn btn-primary">Posta</button>
+                  </div>
+                </div>
             </div>`;
-    for (let i = 0; i < post_data.length; i++){
-        section += `
+  for (let i = 0; i < post_data.length; i++) {
+    section += `
     
             <div class="card-body">
               <div class="d-flex justify-content-between p-2 px-3">
@@ -53,10 +56,10 @@ function generatePost(post_data){
                     class="fa fa-ellipsis-h"></em> </div>
               </div>
               <div class="px-4 mt-3 mb-3">`
-        if(post_data[i]["image"] != null){
-            section += `<img src="${post_data[i]["image"]}" alt="" class="img-fluid">`;
-        }
-        section +=  `
+    if (post_data[i]["image"] != null) {
+      section += `<img src="${post_data[i]["image"]}" alt="" class="img-fluid">`;
+    }
+    section += `
                 <p class="text-justify">${post_data[i]["string"]}.</p>
               </div>
               <div class="d-flex align-items-center mt-4">
@@ -71,68 +74,68 @@ function generatePost(post_data){
             <hr/>
           
         `
-    }
+  }
 
-    section += `</div>
+  section += `</div>
     </div>
   </div>
 </div>
 </div> `;
 
-    const variabile = document.createElement("section");
-    variabile.innerHTML = section;
-    return variabile;
+  const variabile = document.createElement("section");
+  variabile.innerHTML = section;
+  return variabile;
 }
 
-function showPost(post_data){
-    main.appendChild(generatePost(post_data));
+function showPost(post_data) {
+  main.appendChild(generatePost(post_data));
 }
 
-function updateButton(response){
-    const btn = document.querySelectorAll('.bottone');
-    const nlikes = document.querySelectorAll('.numeroLike');
+function updateButton(response) {
+  const btn = document.querySelectorAll('.bottone');
+  const nlikes = document.querySelectorAll('.numeroLike');
 
-    console.log(btn);
+  //console.log(btn);
 
-    const formData = new FormData();
+  const formData = new FormData();
 
-    for(let i = 0; i < btn.length; i++){
-        showAlreadyLiked(response, i, btn, nlikes);
-        btn[i].addEventListener('click', function onClick() {
-            if(btn[i].classList.contains("btnlkd")){
-                removeLike(btn, i, nlikes);
-                formData.append('post_id', response[i]["post_id"]);
-                formData.append('type', -1);
-                console.log(formData);
-                axios.post('../php/like.php', formData).then(response => {
-                    console.log(response);
-                });
-                formData.delete('post_id');
-                formData.delete('type');
-            } else {
-                addLike(btn, i, nlikes);
-                formData.append('post_id', response[i]["post_id"]);
-                formData.append('type', 1);
-                for (const value of formData.values()) {
-                    console.log(value);
-                }
-                axios.post('../php/like.php', formData).then(response => {
-                    console.log(response);
-                });
-
-                formData.delete('post_id');
-                formData.delete('user');
-                formData.delete('type');
-            }
+  for (let i = 0; i < btn.length; i++) {
+    showAlreadyLiked(response, i, btn, nlikes);
+    btn[i].addEventListener('click', function onClick() {
+      if (btn[i].classList.contains("btnlkd")) {
+        removeLike(btn, i, nlikes);
+        formData.append('post_id', response[i]["post_id"]);
+        formData.append('type', -1);
+        //console.log(formData);
+        axios.post('../php/like.php', formData).then(response => {
+          console.log(response);
         });
-    }   
+        formData.delete('post_id');
+        formData.delete('type');
+      } else {
+        addLike(btn, i, nlikes);
+        formData.append('post_id', response[i]["post_id"]);
+        formData.append('type', 1);
+        for (const value of formData.values()) {
+          //console.log(value);
+        }
+        axios.post('../php/like.php', formData).then(response => {
+          console.log(response);
+        });
+
+        formData.delete('post_id');
+        formData.delete('user');
+        formData.delete('type');
+      }
+    });
+  }
 }
 
 function addLike(btn, i, nlikes) {
   btn[i].classList.replace("btn-outline-danger", 'btn-danger');
   btn[i].classList.replace("bottone", "btnlkd");
   nlikes[i].innerHTML = parseInt(nlikes[i].innerHTML) + 1;
-  btn[i].innerHTML = "Liked" + `<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" id="numeroLike${i}" >`+nlikes[i].innerHTML+`</span>`;
+  btn[i].innerHTML = "Liked" + `<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" id="numeroLike${i}" >` + nlikes[i].innerHTML + `</span>`;
 }
 
 function removeLike(btn, i, nlikes) {
@@ -152,9 +155,30 @@ function showAlreadyLiked(response, i, btn, nlikes) {
 
 const main = document.querySelector("main");
 axios.get("api-showpost.php").then(response => {
-    console.log(response.data);
-    showPost(response.data);
-
-    updateButton(response.data);
-    
+  //console.log(response.data);
+  showPost(response.data);
+  updateButton(response.data);
+  sendPost();
 });
+
+function sendPost(){
+  const btnpost = document.querySelector(".bttnpost");
+  btnpost.addEventListener('click', function onClick() {
+    const qpost = document.querySelector(".quickpost").value;
+    if (qpost != "") {
+      console.log(qpost);
+      const formData = new FormData();
+      formData.append('post', qpost);
+      axios.post('../php/quickpost.php', formData).then(response => {
+        if(!response.data["error"]){
+          location.reload();
+          console.log(response.data["info"]);
+        } else {
+          console.log(response.data["info"]);
+        }
+      });
+    } else {
+      console.log("Post is empty");
+    }
+  });
+}
---
+++ source/php/quickpost.php
@@ -0,0 +1,23 @@
+<?php
+require_once("db_config.php");
+
+$error["error"] = true;
+
+if(isset($_POST["post"])){
+    $testo = $_POST["post"];
+    if ($testo != "" && isset($_SESSION["user_id"])) {
+        $dbh->addPost($testo, $_SESSION["user_id"]);
+        $error["error"] = false;
+        $error["info"] = "Post inserito con successo";
+    } else {
+        $error["info"] = "Errore; Devi essere loggato per poter inserire un post";
+        $error["error"] = true;
+
+    }
+}
+
+
+header("Content-Type: application/json");
+echo (json_encode($error));
+
+?>

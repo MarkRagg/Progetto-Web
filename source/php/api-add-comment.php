d8532950e108f83b4e430506ee3bbda1698b49ae
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2023-02-03 11:18:20 +0100

    add notification for comments

--- source/php/api-add-comment.php
+++ source/php/api-add-comment.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once("db_config.php");
+define("COMMENT", 3);
 
 if(isset($_POST['comment']) && isset($_SESSION["user_id"]) && isset($_POST['post_id'])) {
     $query = $dbh->addComment($_POST['comment'], $_POST['post_id'], $_SESSION["user_id"]);
@@ -9,6 +10,10 @@ if(isset($_POST['comment']) && isset($_SESSION["user_id"]) && isset($_POST['post
             "status" => "success",
             "message" => "Commento aggiunto con successo"
         );
+        $author = $dbh->getPost($_POST['post_id'])['author'];
+        if($author!= $_SESSION['user_id']) {
+            $dbh->addNotification($_SESSION['user_id'], $author, $_POST['post_id'], COMMENT);
+        }
     } else {
         $result = array(
             "status" => "error",
@@ -25,5 +30,4 @@ if(isset($_POST['comment']) && isset($_SESSION["user_id"]) && isset($_POST['post
 
 header("Content-Type: application/json");
 echo json_encode($result);
-
 ?>

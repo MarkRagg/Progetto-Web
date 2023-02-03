c6d3326b43bf2e0b2c5d461119e50e60b34acd80
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-02-03 20:32:20 +0100

    add more reaction

--- source/php/api-showpost.php
+++ source/php/api-showpost.php
@@ -8,11 +8,17 @@ $post = $dbh->getPosts($numeropost);
 
 for($i = 0; $i < count($post); $i++){
     $post[$i]["data"] = date("F j, Y", strtotime($post[$i]["data"]));
+    $post[$i]["num_comments"] = $dbh->getPostComments($post[$i]["post_id"]);
+    
     $post[$i]["num_like"] = $dbh->getPostLikes($post[$i]["post_id"]);
     $post[$i]["num_fire"] = $dbh->getPostReactionInfo($post[$i]["post_id"], 2);
-    $post[$i]["num_comments"] = $dbh->getPostComments($post[$i]["post_id"]);
+    $post[$i]["num_smile"] = $dbh->getPostReactionInfo($post[$i]["post_id"], 3);
+    $post[$i]["num_cuore"] = $dbh->getPostReactionInfo($post[$i]["post_id"], 4);
+    
     $post[$i]["user_has_liked"] = $dbh->hasReacted($post[$i]["post_id"], $_SESSION["user_id"], 1);
     $post[$i]["user_has_fired"] = $dbh->hasReacted($post[$i]["post_id"], $_SESSION["user_id"], 2);
+    $post[$i]["user_has_smile"] = $dbh->hasReacted($post[$i]["post_id"], $_SESSION["user_id"], 3);
+    $post[$i]["user_has_cuore"] = $dbh->hasReacted($post[$i]["post_id"], $_SESSION["user_id"], 4);
 }
 
 $templateParams["title"] = "Show Post";

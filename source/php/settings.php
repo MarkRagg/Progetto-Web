446faa785c317b7e3d203693b7d9581c716abf26
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2023-02-04 15:16:22 +0100

    create empty files for settings

---
+++ source/php/settings.php
@@ -0,0 +1,8 @@
+<?php
+require_once("db_config.php");
+
+$templateParams["title"] = "Settings";
+$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js", "../js/settings.js");
+
+require("../template/base.php");
+?>

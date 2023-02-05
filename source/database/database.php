4b9cf3c813f205ad2e6a977da662486844f3a492
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-02-06 00:15:25 +0100

    posts shown by date desc

--- source/database/database.php
+++ source/database/database.php
@@ -89,7 +89,7 @@ class DatabaseManager {
      * returns n posts
      */
     public function getPosts($id, $n) {
-        $stmt = $this->db->prepare("SELECT post.*, user_info.user_image FROM user_followers_followed LEFT JOIN post ON post.author = user_followers_followed.user_id LEFT JOIN user_info on post.author=user_info.user_id WHERE user_followers_followed.follower_id = ? limit ?;");
+        $stmt = $this->db->prepare("SELECT post.*, user_info.user_image FROM user_followers_followed LEFT JOIN post ON post.author = user_followers_followed.user_id LEFT JOIN user_info on post.author=user_info.user_id WHERE user_followers_followed.follower_id = ? ORDER by post.data DESC limit ?;");
         $stmt->bind_param("si",$id, $n);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -438,7 +438,7 @@ class DatabaseManager {
     }
 
     public function getMorePosts($id, $num, $from){
-        $stmt = $this->db->prepare("SELECT post.*, user_info.user_image FROM user_followers_followed LEFT JOIN post ON post.author = user_followers_followed.user_id LEFT JOIN user_info on post.author=user_info.user_id WHERE user_followers_followed.follower_id = ? limit ? OFFSET ?");
+        $stmt = $this->db->prepare("SELECT post.*, user_info.user_image FROM user_followers_followed LEFT JOIN post ON post.author = user_followers_followed.user_id LEFT JOIN user_info on post.author=user_info.user_id WHERE user_followers_followed.follower_id = ? ORDER by post.data DESC limit ? OFFSET ?;");
         $stmt->bind_param("sss", $id, $num, $from);
         $stmt->execute();
         $result = $stmt->get_result();

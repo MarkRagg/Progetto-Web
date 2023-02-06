e7548d80bdd60636be238327917ee1d3df58c17e
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-02-06 15:27:36 +0100

    add last reaction

--- source/js/reactions.js
+++ source/js/reactions.js
@@ -12,6 +12,8 @@ function updateButton(response, idbtn, idnum, numeroin, numeroout, contains) {
             showAlreadySmile(response, i, btn, nlikes);
         } else if (numeroin == 4) {
             showAlreadyCuore(response, i, btn, nlikes);
+        } else if (numeroin == 5) {
+            showAlreadyBacio(response, i, btn, nlikes);
         }
         if (!btn[i].classList.contains("added")) {
             btn[i].classList.add("added");
@@ -25,6 +27,8 @@ function updateButton(response, idbtn, idnum, numeroin, numeroout, contains) {
                         changeReaction(btn, i, nlikes, "btnSmileLkd", "btnSmile", -1);
                     } else if (numeroout == -4) {
                         changeReaction(btn, i, nlikes, "btnCuoreLkd", "btnCuore", -1);
+                    } else if(numeroout == -5) {
+                        changeReaction(btn, i, nlikes, "btnBacioLkd", "btnBacio", -1);
                     }
 
                     removeReaction(formData, response, i, numeroout);
@@ -37,6 +41,8 @@ function updateButton(response, idbtn, idnum, numeroin, numeroout, contains) {
                         changeReaction(btn, i, nlikes, "btnSmile", "btnSmileLkd", 1);
                     } else if (numeroin == 4) {
                         changeReaction(btn, i, nlikes, "btnCuore", "btnCuoreLkd", 1);
+                    } else if (numeroin == 5) {
+                        changeReaction(btn, i, nlikes, "btnBacio", "btnBacioLkd", 1);
                     }
 
                     addReaction(formData, response, i, numeroin);
@@ -104,6 +110,13 @@ function showAlreadyCuore(response, i, btn) {
     if (response[i]["user_has_cuore"] == true) {
         btn[i].classList.replace("btn-outline-danger", "btn-danger");
         btn[i].classList.replace("btnCuore", "btnCuoreLkd");
+    }
+}
+
+function showAlreadyBacio(response, i, btn) {
+    if (response[i]["user_has_bacio"] == true) {
+        btn[i].classList.replace("btn-outline-danger", "btn-danger");
+        btn[i].classList.replace("btnBacio", "btnBacioLkd");
     }
 }
 

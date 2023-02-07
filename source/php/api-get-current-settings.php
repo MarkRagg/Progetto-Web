ed051558a1d557bd17b1ed6dce89a068ded9fa4c
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2023-02-07 15:33:21 +0100

    fix bug

--- source/php/api-get-current-settings.php
+++ source/php/api-get-current-settings.php
@@ -6,10 +6,6 @@ $result["logged"] = false;
 if(isset($_SESSION["user_id"])) {
   $user_id_info = $dbh->getUserInfo($_SESSION["user_id"]);
   $result["bio"] = $user_id_info["descrizione"];
-  $result["user_current_course"] = $user_id_info["corso_id"];
-  if($user_id_info["corso_id"] != null) {
-    $result["user_current_uni"] = $dbh->getUniFromCourse($user_id_info["courso_id"]);
-  }
   $result["unis"] = $dbh->getAllUnis();
   $result["courses"] = $dbh->getCoursesFromUni($result["unis"][0]);
   $result["logged"] = true;
--- source/php/api-selector-controller.php
+++ source/php/api-selector-controller.php
@@ -6,7 +6,9 @@ if(isset($_POST["uni-selected"])) {
   $result["uni-selected"] = $_POST["uni-selected"];
   $result["courses"] = $dbh->getCoursesFromUni($_POST["uni-selected"]);
 } else {
-  $result["courses"] = $dbh->getCoursesFromUni($result["unis"][0]);
+  $result["course-selected"] = $dbh->getUserInfo($_SESSION["user_id"])["corso_id"];
+  $result["uni-selected"] = $dbh->getUniFromCourse($result["course-selected"]);
+  $result["courses"] = $dbh->getCoursesFromUni($result["uni-selected"]);
 }
 
 header("Content-Type: application/json");

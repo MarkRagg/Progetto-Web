4cac567904c5882fbcdf80919ebf1bc8e5f20a44
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2023-02-07 16:04:07 +0100

    fix index

--- source/php/index.php
+++ source/php/index.php
@@ -1,9 +1,12 @@
 <?php
 require_once 'db_config.php';
 
-//Base Template
-$templateParams["title"] = "Sign-in";
-$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","../js/sign-in.js");
+if(!isset($_SESSION["user_id"])) {
+  $templateParams["title"] = "Sign-in";
+  $templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","../js/sign-in.js");
+  require '../template/Login-base.php';
+} else {
+  header("location:showhomepage.php");
+}
 
-require '../template/Login-base.php';
 ?>

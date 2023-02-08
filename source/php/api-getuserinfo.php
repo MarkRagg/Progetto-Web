ab4dff6cc8b8543310c7d57cd614d9b35d4d47c3
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-02-08 11:59:44 +0100

    Update api-getuserinfo.php

--- source/php/api-getuserinfo.php
+++ source/php/api-getuserinfo.php
@@ -6,8 +6,13 @@ if(isset($_SESSION["user_id"]))
 {
     $result["user_info"] = $dbh->getUserInfo($_SESSION["user_id"]);
     $result["course_info"] = $dbh->getCourseInfo($result["user_info"]["corso_id"]);
-    $uni_id = $dbh->getUniFromCourse($result["course_info"]["corso_id"]);
-    $result["uni_info"] = $dbh->getUniInfo($uni_id);
+    if ($result["course_info"]["corso_id"] != null){
+        $uni_id = $dbh->getUniFromCourse($result["course_info"]["corso_id"]);
+        $result["uni_info"] = $dbh->getUniInfo($uni_id);
+    } else {
+        $result["uni_info"] = null;
+    }
+    
     $result["userid"]= $_SESSION["user_id"];
 }
 

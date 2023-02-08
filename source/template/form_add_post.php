23ff6ced1864544a3ebe6b203673890ffa4e0f17
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-02-08 17:37:35 +0100

    fix for mobile first

--- source/template/form_add_post.php
+++ source/template/form_add_post.php
@@ -1,22 +1,20 @@
-<section>
-    <div class="d-flex justify-content-center">
-    <div class="flex-column border bg-light">
+<div class="container mt-2 mb-5">
+    <div class="d-flex justify-content-between border bg-light">
         <div class="p-5 text-center">
             <p class="text-danger"> <?php echo $error; ?> </p>
             <form action="insertpost.php" method="POST" enctype="multipart/form-data">
-            <h2><strong><label for="post">Il Tuo Nuovo Post:</label></strong></h2>
-                <hr/>
-                <label for="imgpost">Aggiungi immagine</label><input type="file" name="imgpost" id="imgpost" />
+                <p class="fs-3"><strong><label for="post">Il Tuo Nuovo Post:</label></strong></p>
+                <hr />
+                <label for="imgpost">Aggiungi immagine</label><input type="file" name="imgpost" id="imgpost" class="form-control"/>
                 <textarea class="form-control" id="post" name="post" rows="4"></textarea>
                 <small id="infoPostLunghezza" class="form-text text-muted">
                     Il post puo' essere lungo massimo 200 caratteri.</small>
-                <hr/>
+                <hr />
                 <div class="d-flex justify-content-end">
-                    <button type="submit" data-toggle="button" class="btn btn-outline-primary" name="submit">Posta</button>
+                    <button type="submit" data-toggle="button" class="btn btn-outline-primary"
+                        name="submit">Posta</button>
                 </div>
             </form>
         </div>
     </div>
-    </div>
-</section>
-
+</div>

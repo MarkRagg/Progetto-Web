ab5b315bf23feac35497dbb2813b26a45b96ab47
Simone Zama <simone.zama@studio.unibo.it>
2023-02-08 16:12:05 +0100

    Fix api

--- source/php/api-getuserinfo.php
+++ source/php/api-getuserinfo.php
@@ -7,7 +7,7 @@ if(isset($_SESSION["user_id"]))
     $result["user_info"] = $dbh->getUserInfo($_SESSION["user_id"]);
     $result["course_info"] = $dbh->getCourseInfo($result["user_info"]["corso_id"]);
     if ($result["course_info"] != null){
-        $uni_id = $dbh->getUniFromCourse($result["course_info"]);
+        $uni_id = $dbh->getUniFromCourse($result["course_info"]["corso_id"]);
         $result["uni_info"] = $dbh->getUniInfo($uni_id);
     } else {
         $result["uni_info"] = null;

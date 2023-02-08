3285f1db58d5bef63797e28f7018a07d16acf3f4
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2023-02-08 12:07:15 +0100

    fix bug

--- source/php/api-getuserinfo.php
+++ source/php/api-getuserinfo.php
@@ -6,8 +6,8 @@ if(isset($_SESSION["user_id"]))
 {
     $result["user_info"] = $dbh->getUserInfo($_SESSION["user_id"]);
     $result["course_info"] = $dbh->getCourseInfo($result["user_info"]["corso_id"]);
-    if ($result["course_info"]["corso_id"] != null){
-        $uni_id = $dbh->getUniFromCourse($result["course_info"]["corso_id"]);
+    if ($result["course_info"] != null){
+        $uni_id = $dbh->getUniFromCourse($result["course_info"]);
         $result["uni_info"] = $dbh->getUniInfo($uni_id);
     } else {
         $result["uni_info"] = null;

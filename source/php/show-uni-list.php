77c89b8b23431bfb529275ef89a6e9d4de848fa2
Simone Zama <simone.zama@studio.unibo.it>
2023-02-09 09:38:33 +0100

    Remove header class and fix positions

--- source/php/show-uni-list.php
+++ source/php/show-uni-list.php
@@ -1,13 +1,13 @@
 <?php foreach($templateParams["uni_list"] as $uni): ?>
 <div class="container bg-white p-3 rounded-3 my-5">
-    <div class="row">
-        <a class="col-6 link-primary h1" href="uni.php?uni_id=<?php echo($uni["uni_id"]) ?>">
+    <div class="row flex align-items-center">
+        <a class="col-6 link-primary" href="uni.php?uni_id=<?php echo($uni["uni_id"]) ?>">
             <?php echo($uni["nome"]) ?>
         </a>
-        <p class="col-3 h2">
+        <p class="col-3 text-center">
             <?php echo($uni["sede"]) ?>
         </p>
-        <p class="col-3 h2 justify-self-end">
+        <p class="col-3 text-center justify-self-end">
             Corsi <span class="badge bg-primary rounded-pill"><?php echo($uni["course_count"]) ?></span>
         </p>
     </div>

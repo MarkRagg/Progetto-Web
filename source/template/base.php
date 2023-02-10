fe659cb49d7d2e93d6bb4fbed77f8a776e858997
Simone Zama <simone.zama@studio.unibo.it>
2023-02-10 13:04:09 +0100

    Chage language of close button

--- source/template/base.php
+++ source/template/base.php
@@ -64,7 +64,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
                 <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
             </div>
             </div>

f531a8413dcd3d5d2ee6e6bd7b2b332ccc67224a
Simone Zama <simone.zama@studio.unibo.it>
2023-02-12 14:58:29 +0100

    Add delete post checkbox

--- source/js/modify-post.js
+++ source/js/modify-post.js
@@ -12,8 +12,48 @@ function removeImageInput() {
     document.getElementById("imgDiv")?.remove();
 }
 
+function toggleDelete() {
+    if (!deleteToggled) {
+        // remove all other forms
+        document.getElementById("formContent")?.remove();
+        deleteToggled = true;
+    } else {
+        // add all forms
+        const formContent = document.createElement("div");
+        formContent.id = "formContent";
+        formContent.innerHTML = `
+            <div class="form-check">
+                <input class="form-check-input" value="sameImage" type="radio" name="image" id="sameImage" checked>
+                <label class="form-check-label" for="sameImage">
+                    Lascia immagine invariata.
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" value="deleteImage" type="radio" name="image" id="deleteImage">
+                <label class="form-check-label" for="deleteImage">
+                    Elimina immagine.
+                </label>
+            </div>
+            <div class="form-check mb-2">
+                <input class="form-check-input" value="changeImage" type="radio" name="image" id="changeImage">
+                <label class="form-check-label" for="changeImage">
+                    Cambia o aggiungi immagine.
+                </label>
+            </div>
+            <textarea class="form-control" id="post" name="post" rows="4">${postContent}</textarea>
+        `;
+        const firstHr = document.getElementById("formContentDivider");
+        console.log(firstHr);
+        firstHr.parentElement.insertBefore(formContent, firstHr.nextElementSibling);
+        deleteToggled = false;
+    }
+}
+
 const changeImage = document.getElementById("changeImage");
 const otherRadios = new Set([document.getElementById("sameImage"), document.getElementById("deleteImage")]);
+const deleteButton = document.getElementById("deletePost");
+const postContent = document.querySelector("textarea").innerText;
+let deleteToggled = false;
 changeImage.addEventListener("click", function() {
     addImageInput();
 });
@@ -21,4 +61,7 @@ otherRadios.forEach(x => {
     x.addEventListener("click", function() {
         removeImageInput();
     });
+});
+deleteButton.addEventListener("click", function(event) {
+    toggleDelete();
 });
--- source/php/show-modify-post.php
+++ source/php/show-modify-post.php
@@ -1,32 +1,36 @@
 <div class="container mt-2 mb-5">
     <div class="justify-content-center border bg-light">
         <div class="p-5">
-            <form action="insertpost.php" method="POST" enctype="multipart/form-data">
+            <form action="modify-post.php?post_id=<?php echo($templateParams["post_info"]["post_id"]) ?>" method="POST" enctype="multipart/form-data">
                 <p class="fs-3 text-center"><strong><label for="post">Modifica il tuo post:</label></strong></p>
+                <hr id="formContentDivider" />
+                <div id="formContent">
+                    <div class="form-check">
+                        <input class="form-check-input" value="sameImage" type="radio" name="image" id="sameImage" checked>
+                        <label class="form-check-label" for="sameImage">
+                            Lascia immagine invariata.
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" value="deleteImage" type="radio" name="image" id="deleteImage">
+                        <label class="form-check-label" for="deleteImage">
+                            Elimina immagine.
+                        </label>
+                    </div>
+                    <div class="form-check mb-2">
+                        <input class="form-check-input" value="changeImage" type="radio" name="image" id="changeImage">
+                        <label class="form-check-label" for="changeImage">
+                            Cambia o aggiungi immagine.
+                        </label>
+                    </div>
+                    <textarea class="form-control" id="post" name="post" rows="4"><?php echo($templateParams["post_info"]["string"]) ?></textarea>
+                </div>
                 <hr />
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="image" id="sameImage" checked>
-                    <label class="form-check-label" for="sameImage">
-                        Lascia immagine invariata.
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="image" id="deleteImage">
-                    <label class="form-check-label" for="deleteImage">
-                        Elimina immagine.
-                    </label>
-                </div>
-                <div class="form-check mb-2">
-                    <input class="form-check-input" type="radio" name="image" id="changeImage">
-                    <label class="form-check-label" for="changeImage">
-                        Cambia o aggiungi immagine.
-                    </label>
-                </div>
-                <textarea class="form-control" id="post" name="post" rows="4"><?php echo($templateParams["post_info"]["string"]) ?></textarea>
-                <hr />
-                <div class="d-flex justify-content-end">
+                <div class="d-flex justify-content-between">
+                    <input type="checkbox" class="btn-check" id="deletePost" autocomplete="off">
+                    <label class="btn btn-danger" for="deletePost">Elimina post</label>
                     <button type="submit" data-toggle="button" class="btn btn-outline-primary"
-                        name="submit">Posta</button>
+                        name="submit">Conferma</button>
                 </div>
             </form>
         </div>
